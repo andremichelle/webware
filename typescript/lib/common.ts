7c9fddf2c0d78d6cc2aeed366e0842f6f516a74f
André Michelle <andre.michelle@gmail.com>
2022-03-15 13:11:07 +0100

    initial commit

---
+++ typescript/lib/common.ts
@@ -0,0 +1,397 @@
+// noinspection JSUnusedGlobalSymbols
+
+import {ValueMapping} from "./mapping.js"
+import {Random} from "./math"
+
+export interface Terminable {
+    terminate(): void
+}
+
+export const TerminableVoid: Terminable = {
+    terminate() {
+    }
+}
+
+export class Terminator implements Terminable {
+    private readonly terminables: Terminable[] = []
+
+    with<T extends Terminable>(terminable: T): T {
+        this.terminables.push(terminable)
+        return terminable
+    }
+
+    terminate(): void {
+        while (this.terminables.length) {
+            this.terminables.pop().terminate()
+        }
+    }
+}
+
+export interface Option<T> {
+    get(): T
+
+    ifPresent(callback: (value: T) => void): void
+
+    contains(value: T): boolean
+
+    isEmpty(): boolean
+
+    nonEmpty(): boolean
+}
+
+export class Options {
+    static valueOf<T>(value: T): Option<T> {
+        return null === value || undefined === value ? Options.None : new Options.Some(value)
+    }
+
+    static Some = class<T> implements Option<T> {
+        constructor(readonly value: T) {
+            console.assert(null !== value && undefined !== value, "Cannot be null or undefined")
+        }
+
+        get = (): T => this.value
+        contains = (value: T): boolean => value === this.value
+        ifPresent = (callback: (value: T) => void): void => callback(this.value)
+        isEmpty = (): boolean => false
+        nonEmpty = (): boolean => true
+
+        toString(): string {
+            return `Options.Some(${this.value})`
+        }
+    }
+
+    static None = new class implements Option<never> {
+        get = (): never => {
+            throw new Error("Option has no value")
+        }
+        contains = (_: never): boolean => false
+        ifPresent = (_: (value: never) => void): void => {
+        }
+        isEmpty = (): boolean => true
+        nonEmpty = (): boolean => false
+
+        toString(): string {
+            return `Options.None`
+        }
+    }
+}
+
+export type Observer<VALUE> = (value: VALUE) => void
+
+export interface Observable<VALUE> extends Terminable {
+    addObserver(observer: Observer<VALUE>, notify: boolean): Terminable
+
+    removeObserver(observer: Observer<VALUE>): boolean
+}
+
+export class ObservableImpl<T> implements Observable<T> {
+    private readonly observers: Observer<T>[] = []
+
+    notify(value: T) {
+        this.observers.forEach(observer => observer(value))
+    }
+
+    addObserver(observer: Observer<T>): Terminable {
+        this.observers.push(observer)
+        return {terminate: () => this.removeObserver(observer)}
+    }
+
+    removeObserver(observer: Observer<T>): boolean {
+        let index = this.observers.indexOf(observer)
+        if (-1 < index) {
+            this.observers.splice(index, 1)
+            return true
+        }
+        return false
+    }
+
+    terminate(): void {
+        this.observers.splice(0, this.observers.length)
+    }
+}
+
+export interface Serializer<T> {
+    serialize(): T
+
+    deserialize(format: T): Serializer<T>
+}
+
+export interface Value<T> {
+    set(value: T): boolean
+
+    get(): T
+}
+
+export interface ObservableValue<T> extends Value<T>, Observable<T> {
+}
+
+export class ObservableValueImpl<T> implements ObservableValue<T> {
+    private readonly observable = new ObservableImpl<T>()
+
+    constructor(private value?: T) {
+    }
+
+    get(): T {
+        return this.value
+    }
+
+    set(value: T): boolean {
+        if (this.value === value) {
+            return false
+        }
+        this.value = value
+        this.observable.notify(value)
+        return true
+    }
+
+    addObserver(observer: Observer<T>, notify: boolean = false): Terminable {
+        if (notify) observer(this.value)
+        return this.observable.addObserver(observer)
+    }
+
+    removeObserver(observer: Observer<T>): boolean {
+        return this.observable.removeObserver(observer)
+    }
+
+    terminate(): void {
+        this.observable.terminate()
+    }
+}
+
+export class Parameter<T> implements ObservableValue<T> {
+    private readonly observable = new ObservableImpl<T>()
+
+    constructor(readonly valueMapping: ValueMapping<T>,
+                readonly printMapping: PrintMapping<T>,
+                private value: T) {
+    }
+
+    getUnipolar(): number {
+        return this.valueMapping.x(this.value)
+    }
+
+    setUnipolar(value: number): void {
+        this.set(this.valueMapping.y(value))
+    }
+
+    print(): string {
+        return this.printMapping.print(this.value)
+    }
+
+    get(): T {
+        return this.value
+    }
+
+    set(value: T): boolean {
+        if (value === this.value) {
+            return
+        }
+        this.value = value
+        this.observable.notify(value)
+        return true
+    }
+
+    addObserver(observer: Observer<T>, notify: boolean = false): Terminable {
+        if (notify) observer(this.value)
+        return this.observable.addObserver(observer)
+    }
+
+    removeObserver(observer: Observer<T>): boolean {
+        return this.observable.removeObserver(observer)
+    }
+
+    terminate(): void {
+        this.observable.terminate()
+    }
+}
+
+export type Parser<Y> = (text: string) => Y | null
+export type Printer<Y> = (value: Y) => string
+
+export class PrintMapping<Y> {
+    static INTEGER = PrintMapping.integer("")
+    static FLOAT_ONE = PrintMapping.float(1, "", "")
+
+    static createBoolean(trueValue: string, falseValue: string) {
+        return new PrintMapping(text => {
+            return trueValue.toLowerCase() === text.toLowerCase()
+        }, value => value ? trueValue : falseValue)
+    }
+
+    static UnipolarPercent = new PrintMapping(text => {
+        const value = parseFloat(text)
+        if (isNaN(value)) return null
+        return value / 100.0
+    }, value => (value * 100.0).toFixed(1), "", "%")
+    static RGB = new PrintMapping<number>(text => {
+        if (3 === text.length) {
+            text = text.charAt(0) + text.charAt(0) + text.charAt(1) + text.charAt(1) + text.charAt(2) + text.charAt(2)
+        }
+        if (6 === text.length) {
+            return parseInt(text, 16)
+        } else {
+            return null
+        }
+    }, value => value.toString(16).padStart(6, "0").toUpperCase(), "#", "")
+
+    static integer(postUnit: string): PrintMapping<number> {
+        return new PrintMapping(text => {
+            const value = parseInt(text, 10)
+            if (isNaN(value)) return null
+            return Math.round(value) | 0
+        }, value => String(value), "", postUnit)
+    }
+
+    static float(numPrecision: number, preUnit: string, postUnit: string): PrintMapping<number> {
+        return new PrintMapping(text => {
+            const value = parseFloat(text)
+            if (isNaN(value)) return null
+            return value
+        }, value => {
+            if (isNaN(value)) {
+                return "N/A"
+            }
+            if (!isFinite(value)) {
+                return value < 0.0 ? "-∞" : "∞"
+            }
+            return value.toFixed(numPrecision)
+        }, preUnit, postUnit)
+    }
+
+    static smallFloat(numPrecision: number, postUnit: string): PrintMapping<number> {
+        return new PrintMapping(text => {
+            const value = parseFloat(text)
+            if (isNaN(value)) return null
+            return text.toLowerCase().includes("k") ? value * 1000.0 : value
+        }, value => {
+            if (value >= 1000.0) {
+                return `${(value / 1000.0).toFixed(numPrecision)}k`
+            }
+            return value.toFixed(numPrecision)
+        }, "", postUnit)
+    }
+
+    constructor(private readonly parser: Parser<Y>,
+                private readonly printer: Printer<Y>,
+                private readonly preUnit = "",
+                private readonly postUnit = "") {
+    }
+
+    parse(text: string): Y | null {
+        return this.parser(text.replace(this.preUnit, "").replace(this.postUnit, ""))
+    }
+
+    print(value: Y): string {
+        return undefined === value ? "" : `${this.preUnit}${this.printer(value)}${this.postUnit}`
+    }
+}
+
+export interface Stepper {
+    decrease(value: ObservableValue<number>): void
+
+    increase(value: ObservableValue<number>): void
+}
+
+export class NumericStepper implements Stepper {
+    static Integer = new NumericStepper(1)
+    static Hundredth = new NumericStepper(0.01)
+
+    constructor(private readonly step: number = 1) {
+    }
+
+    decrease(value: ObservableValue<number>): void {
+        value.set(Math.round((value.get() - this.step) / this.step) * this.step)
+    }
+
+    increase(value: ObservableValue<number>): void {
+        value.set(Math.round((value.get() + this.step) / this.step) * this.step)
+    }
+}
+
+export class ArrayUtils {
+    static fill<T>(n: number, factory: (index: number) => T): T[] {
+        const array: T[] = []
+        for (let i = 0; i < n; i++) {
+            array[i] = factory(i)
+        }
+        return array
+    }
+
+    static shuffle(array: ArrayBufferLike, n: number, random: Random) {
+        for (let i = 0; i < n; i++) {
+            const element = array[i]
+            const randomIndex = random.nextInt(0, n - 1)
+            array[i] = array[randomIndex]
+            array[randomIndex] = element
+        }
+    }
+
+    static binarySearch = (array: ArrayBufferLike, length: number, key: number): number => {
+        let low = 0 | 0
+        let high = (length - 1) | 0
+        while (low <= high) {
+            const mid = (low + high) >>> 1
+            const midVal = array[mid]
+            if (midVal < key)
+                low = mid + 1
+            else if (midVal > key)
+                high = mid - 1
+            else {
+                if (midVal === key)
+                    return mid
+                else if (midVal < key)
+                    low = mid + 1
+                else
+                    high = mid - 1
+            }
+        }
+        return high
+    }
+
+    // noinspection JSUnusedLocalSymbols
+    private constructor() {
+    }
+}
+
+export interface SettingsFormat<DATA> {
+    class: string
+    data: DATA
+}
+
+export abstract class Settings<DATA> implements Observable<Settings<DATA>>, Serializer<SettingsFormat<DATA>>, Terminable {
+    protected readonly terminator: Terminator = new Terminator()
+    protected readonly observable: ObservableImpl<Settings<DATA>> = new ObservableImpl<Settings<DATA>>()
+
+    abstract deserialize(format: SettingsFormat<DATA>): Settings<DATA>
+
+    abstract serialize(): SettingsFormat<DATA>
+
+    protected pack(data?: DATA): SettingsFormat<DATA> {
+        return {
+            class: this.constructor.name,
+            data: data
+        }
+    }
+
+    protected unpack(format: SettingsFormat<DATA>): DATA {
+        console.assert(this.constructor.name === format.class)
+        return format.data
+    }
+
+    protected bindValue<T>(property: ObservableValue<T>): ObservableValue<T> {
+        this.terminator.with(property.addObserver(() => this.observable.notify(this), false))
+        return this.terminator.with(property)
+    }
+
+    addObserver(observer: Observer<Settings<DATA>>): Terminable {
+        return this.observable.addObserver(observer)
+    }
+
+    removeObserver(observer: Observer<Settings<DATA>>): boolean {
+        return this.observable.removeObserver(observer)
+    }
+
+    terminate(): void {
+        this.terminator.terminate()
+    }
+}
---
+++ typescript/main.ts
@@ -0,0 +1,35 @@
+import {Boot, newAudioContext, preloadImagesOfCssFile} from "./lib/boot.js"
+import {LimiterWorklet} from "./audio/limiter/worklet.js"
+import {MeterWorklet} from "./audio/meter/worklet.js"
+import {MetronomeWorklet} from "./audio/metronome/worklet.js"
+
+const showProgress = (() => {
+    const progress: SVGSVGElement = document.querySelector("svg.preloader")
+    console.log(progress)
+    window.onerror = () => progress.classList.add("error")
+    window.onunhandledrejection = () => progress.classList.add("error")
+    return (percentage: number) => progress.style.setProperty("--percentage", percentage.toFixed(2))
+})();
+
+(async () => {
+    console.log("booting...")
+
+    // --- PREPARATION STARTS ---
+
+    const context = newAudioContext()
+    const boot = new Boot()
+    boot.addObserver(boot => showProgress(boot.normalizedPercentage()))
+    boot.registerProcess(preloadImagesOfCssFile("./bin/main.css"))
+    boot.registerProcess(LimiterWorklet.loadModule(context))
+    boot.registerProcess(MeterWorklet.loadModule(context))
+    boot.registerProcess(MetronomeWorklet.loadModule(context))
+    await boot.waitForCompletion()
+
+    // --- PREPARATION ENDS ---
+
+    // prevent dragging entire document on mobile
+    document.addEventListener('touchmove', (event: TouchEvent) => event.preventDefault(), {passive: false})
+    document.querySelectorAll("body main").forEach(element => element.classList.remove("invisible"))
+    document.querySelectorAll("body svg.preloader").forEach(element => element.remove())
+    console.log("boot complete.")
+})()
