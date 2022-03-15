5e97747757f431f5e49970d58747e8e6bbc0066e
André Michelle <andre.michelle@gmail.com>
2022-03-15 13:43:25 +0100

    test grid

--- typescript/main.ts
+++ typescript/main.ts
@@ -5,31 +5,30 @@ import {MetronomeWorklet} from "./audio/metronome/worklet.js"
 
 const showProgress = (() => {
     const progress: SVGSVGElement = document.querySelector("svg.preloader")
-    console.log(progress)
     window.onerror = () => progress.classList.add("error")
     window.onunhandledrejection = () => progress.classList.add("error")
     return (percentage: number) => progress.style.setProperty("--percentage", percentage.toFixed(2))
 })();
 
 (async () => {
-    console.log("booting...")
+    console.debug("booting...")
 
-    // --- PREPARATION STARTS ---
+    // --- BOOT STARTS ---
 
-    const context = newAudioContext()
     const boot = new Boot()
     boot.addObserver(boot => showProgress(boot.normalizedPercentage()))
     boot.registerProcess(preloadImagesOfCssFile("./bin/main.css"))
+    const context = newAudioContext()
     boot.registerProcess(LimiterWorklet.loadModule(context))
     boot.registerProcess(MeterWorklet.loadModule(context))
     boot.registerProcess(MetronomeWorklet.loadModule(context))
     await boot.waitForCompletion()
 
-    // --- PREPARATION ENDS ---
+    // --- BOOT ENDS ---
 
     // prevent dragging entire document on mobile
     document.addEventListener('touchmove', (event: TouchEvent) => event.preventDefault(), {passive: false})
     document.querySelectorAll("body main").forEach(element => element.classList.remove("invisible"))
     document.querySelectorAll("body svg.preloader").forEach(element => element.remove())
-    console.log("boot complete.")
+    console.debug("boot complete.")
 })()
