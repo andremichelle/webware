40cb47aea287389082845c128360ac6e3e43ff27
André Michelle <andre.michelle@gmail.com>
2022-03-19 17:00:32 +0100

    exports svg method

--- typescript/lib/svg.ts
+++ typescript/lib/svg.ts
@@ -1,2 +1,2 @@
-const createElement = <K extends keyof SVGElementTagNameMap>(name: K)
+export const createElement = <K extends keyof SVGElementTagNameMap>(name: K)
     : SVGElementTagNameMap[K] => document.createElementNS("http://www.w3.org/2000/svg", name)
